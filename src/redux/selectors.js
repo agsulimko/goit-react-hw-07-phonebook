2f0f59fd564c767e176ba3168a668660a5f514e6
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-10-13 22:54:57 +0200

    u

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,16 @@
 export const selectContacts = state => state.contacts;
 export const selectFilter = state => state.filter;
+
+// export const getIsLoading = state => state.contacts.isLoading;
+// export const getError = state => state.contacts.error;
+
+// import { createSelector } from '@reduxjs/toolkit';
+// export const selectFilteredContacts = createSelector(
+//   [selectContacts, selectFilter],
+//   (contacts, filter) => {
+//     const normalizedValue = filter.toLowerCase().trim();
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizedValue)
+//     );
+//   }
+// );
